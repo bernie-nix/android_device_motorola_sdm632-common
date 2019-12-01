9c7b51a79f2942e22fc0296626a2425be0a83551
Joshua Blanchard <joshua.lee.bbg@gmail.com>
2019-12-01 00:23:22 -0500

    sdm632-common: Build libvndfwk_detect_jni.qti
    * Build both core and vendor versions
    
    Change-Id: I527762fbbd0cce0aeee4017af08bcd502cba6044
    Signed-off-by: Joshua Blanchard <joshua.lee.bbg@gmail.com>

--- common.mk
+++ common.mk
@@ -346,7 +346,9 @@ PRODUCT_COPY_FILES += \
 
 # QCOM
 PRODUCT_PACKAGES += \
-    libqti_vndfwk_detect
+    libqti_vndfwk_detect \
+    libvndfwk_detect_jni.qti \
+    libvndfwk_detect_jni.qti.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/telephony_product_privapp-permissions-qti.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/telephony_product_privapp-permissions-qti.xml \
